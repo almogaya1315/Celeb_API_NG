f032dd75ae49e19c1370c26b65b1ab91ffe856f8
almogaya1315 <liormatsliah1985@gmail.com>
2017-05-19 08:14:03 +0300

    misc.

--- UI/App/Celebs/CelebsListCtrl.js
+++ UI/App/Celebs/CelebsListCtrl.js
@@ -1,72 +1,13 @@
 ﻿
 (function () {
-    app.controller("CelebsListCtrl", ["$scope", "$http", CelebsListCtrl]);
+    app.controller("CelebsListCtrl", ["$scope", "RequestService", CelebsListCtrl]);
 
-    function CelebsListCtrl($scope, $http) {
-
+    function CelebsListCtrl($scope, RequestService) {
         var vm = this;
-        var baseUrl = "http://localhost:56399/api/celebs";
 
-        $http({
-            method: "get",
-            url: baseUrl,
-        }).then(function (response) {
-            vm.celebs = response.data;
-        });
+        RequestService.initial();
 
-        vm.create = function () {
-            vm.isCreate = !vm.isCreate;
-        }
-
-        vm.save = function () {
-            var c;
-            for (var i = 0; i < vm.celebs.length; i++) {
-                if (vm.celebs[i].id == vm.celeb.id)
-                    c = vm.celebs[i];
-            }
-
-            if (c) {
-                c = vm.celeb;
-                var putUrl = baseUrl + "/" + c.id;
-                var data = { "name": c.name, "age": c.age, "country": c.country }
-                $http({
-                    method: "put",
-                    url: putUrl,
-                    data: data
-                }).then(vm.InitPage());
-            }
-            else {
-                vm.celebs.push(vm.celeb);
-
-                $http({
-                    method: "post",
-                    url: baseUrl,
-                    data: vm.celeb
-                }).then(vm.InitPage())
-            }
-        }
-
-        vm.InitPage = function () {
-            vm.celeb = null;
-            vm.create();
-        }
-
-
-        vm.del = function (index, id) {
-            var delUrl = baseUrl + "/" + id;
-            $http({
-                method: "delete",
-                url: delUrl
-            });
-            vm.celebs.splice(index, 1);
-        }
-
-        vm.put = function (celeb) {
-            if (!vm.isCreate) {
-                vm.isCreate = true;
-            }
-            vm.celeb = angular.copy(celeb);
-        }
+        vm.create = RequestService.create();
 
         $scope.$watch("vm.celeb", function (newValue) {
             vm.celeb = newValue;
--- UI/Services/RequestService.js
+++ UI/Services/RequestService.js
@@ -1,4 +1,72 @@
 ﻿
 (function () {
+    app.factory("RequestService", ["$http", ReqOperations]);
 
+    function ReqOperations($http) {
+
+        var baseUrl = "http://localhost:56399/api/celebs";
+
+        function initial() {
+            $http({
+                method: "get",
+                url: baseUrl,
+            }).then(function (response) {
+                vm.celebs = response.data;
+            })
+        };
+
+        function create () {
+            return  // vm.isCreate = !vm.isCreate;
+        }
+
+        vm.save = function () {
+            var c;
+            for (var i = 0; i < vm.celebs.length; i++) {
+                if (vm.celebs[i].id == vm.celeb.id)
+                    c = vm.celebs[i];
+            }
+
+            if (c) {
+                c = vm.celeb;
+                var putUrl = baseUrl + "/" + c.id;
+                var data = { "name": c.name, "age": c.age, "country": c.country }
+                $http({
+                    method: "put",
+                    url: putUrl,
+                    data: data
+                }).then(vm.InitPage());
+            }
+            else {
+                vm.celebs.push(vm.celeb);
+
+                $http({
+                    method: "post",
+                    url: baseUrl,
+                    data: vm.celeb
+                }).then(vm.InitPage())
+            }
+        }
+
+        vm.InitPage = function () {
+            vm.celeb = null;
+            vm.create();
+        }
+
+
+        vm.del = function (index, id) {
+            var delUrl = baseUrl + "/" + id;
+            $http({
+                method: "delete",
+                url: delUrl
+            });
+            vm.celebs.splice(index, 1);
+        }
+
+        vm.put = function (celeb) {
+            if (!vm.isCreate) {
+                vm.isCreate = true;
+            }
+            vm.celeb = angular.copy(celeb);
+        }
+    }
 }());
